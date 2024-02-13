f0c882d505a76f4a455dc3b852e5501b05ff8fa2
plaristote <michael@unetresgrossebite.com>
2024-02-13 12:51:27 +0100

    rewrite vector<string> trait using iomanip's std::quoted

--- libcrails-odb/crails/odb/traits/vector_string.hpp
+++ libcrails-odb/crails/odb/traits/vector_string.hpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <sstream>
 #include <string>
+#include <iomanip>
 
 namespace odb
 {
@@ -23,16 +24,20 @@ namespace odb
         v.clear();
         if (!is_null)
         {
-          const std::string s(b.data(), n);
-          for (std::size_t i = 0 ; i < s.length() ;)
+          std::string_view source(b.data(), n);
+
+          if (source[0] == '{' && *source.rbegin() == '}')
           {
-            switch (s[i])
+            std::istringstream ss(std::string(&source[1], source.length() - 1));
+            std::string current_value;
+            char end_char;
+
+            while (ss.rdbuf()->in_avail())
             {
-            case '"':
-              v.push_back(output_escaped_word(&s[i], s.length() - i, i));
-              break ;
-            case '}':
-              return ;
+              ss >> std::quoted(current_value)
+                 >> end_char;
+              v.push_back(current_value);
+              if (end_char != ',') break ;
             }
           }
         }
@@ -43,15 +48,14 @@ namespace odb
                              bool& is_null,
                              const value_type& v)
       {
-        is_null = false;
         std::ostringstream os;
+
+        is_null = false;
         os << '{';
         for (value_type::const_iterator i(v.begin()), e(v.end());
              i != e;)
         {
-          os << '"';
-          input_escaped_word(os, i->c_str(), i->length());
-          os << '"';
+          os << std::quoted(*i);
           if (++i != e) os << ',';
         }
         os << '}';
@@ -60,41 +64,6 @@ namespace odb
         if (n > b.capacity())
           b.capacity(n);
         std::memcpy(b.data(), s.c_str(), n);
-      }
-
-    private:
-      static void input_escaped_word(std::ostringstream& os, const char* word, std::size_t length)
-      {
-        for (std::size_t i = 0 ; i != length ; ++i)
-        {
-          char c = word[i];
-          if (c == '"' || c == '\\') os << '\\';
-          os << c;
-        }
-      }
-
-      static std::string output_escaped_word(const char* buffer, std::size_t length, std::size_t& walked)
-      {
-        char c;
-        bool escaping = false;
-        std::ostringstream word;
-        std::size_t i = 0;
-
-        for (; i < length ; ++i)
-        {
-          char c = buffer[i];
-          if (c == '\\' && !escaping)
-            escaping = true;
-          else if (c == '"' && !escaping)
-            break ;
-          else
-          {
-            word << c;
-            escaping = false;
-          }
-        }
-        walked += i + 1;
-        return word.str();
       }
     };
   }
